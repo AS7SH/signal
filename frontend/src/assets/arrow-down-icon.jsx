f320adc9b1d83018857e2c2fbb62f7b87381a360
asish <asishathota@gmail.com>
2026-08-26 17:12:40 +0530

    added some cool icons

---
+++ frontend/src/assets/arrow-down-icon.jsx
@@ -0,0 +1,20 @@
+const ArrowDownIcon = ({ className }) => {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            height="20"
+            width="20"
+            preserveAspectRatio="xMidYMid meet"
+            className={className}
+            fill="currentColor"
+        >
+            <title>ic-expand-more</title>
+            <path
+                fill="currentColor"
+                d="M12 14.95a1.1 1.1 0 0 1-.38-.06.88.88 0 0 1-.32-.21l-4.6-4.6a.95.95 0 0 1-.28-.7.95.95 0 0 1 .98-.97c.28-.01.52.08.7.27l3.9 3.9 3.9-3.9a.95.95 0 0 1 .7-.28.95.95 0 0 1 .97.97c0 .29-.09.52-.27.7l-4.6 4.6c-.1.1-.2.18-.32.22a1.1 1.1 0 0 1-.38.06Z"
+            ></path>
+        </svg>
+    );
+};
+
+export default ArrowDownIcon;
---
+++ frontend/src/assets/close-icon.jsx
@@ -0,0 +1,20 @@
+const CloseIcon = ({ className }) => {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={className}
+        >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+        </svg>
+    );
+};
+
+export default CloseIcon;
